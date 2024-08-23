6d5c50acdb7281ceaa3666d7afb47dbe2982214c
Mahmoud Refai <98220943+hameedorefai@users.noreply.github.com>
2024-08-23 20:13:29 +0300

    Update clsDataAccessSettings.cs

--- DataAccess/clsDataAccessSettings.cs
+++ DataAccess/clsDataAccessSettings.cs
@@ -2,6 +2,8 @@
 {
     internal class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=ZamaylOnlineExams;User Id=sa;Password=sa123456;TrustServerCertificate=True;";
+        //public static string ConnectionString = "Server=.;Database=ZamaylOnlineExams;User Id=sa;Password=sa123456;TrustServerCertificate=True;";
+        public static string ConnectionString = "Server=SQL6032.site4now.net;Database=db_aab694_zamaylonlineexams;User Id=db_aab694_zamaylonlineexams_admin;Password=sa123456;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
     }
 }
